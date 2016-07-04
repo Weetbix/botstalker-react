dfa8fab696483728c13709c9f9d086ba7bcecd5f
John Hannagan <weetbix_@hotmail.com>
2016-07-05 00:50:34 +0200

    Add basic view of messages in chat

--- src/components/ChannelPage.jsx
+++ src/components/ChannelPage.jsx
@@ -1,11 +1,32 @@
-import React, { Component } from 'react'
+import React, { Component, PropTypes } from 'react';
+import { connect } from 'react-redux';
 
-export default class extends Component {
+class ChannelPage extends Component {
+    PropTypes: {
+        messages : PropTypes.array.required
+    }
+
     render() {
         return (
             <div>
-                Channel List
+            { this.props.messages.map( msg => <div>{ msg.text }</div>) }
             </div>
         );
     }
 }
+
+
+function mapStateToProps(state, ownProps){
+    const channelID = ownProps.params.channel_id;
+    
+    return {
+        messages : state.messagesByChannel[channelID].messages,
+        isLoading : state.messagesByChannel[channelID].isFetching
+    }
+}
+
+function mapDispatchToProps(dispatch){
+  return {};
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(ChannelPage);
