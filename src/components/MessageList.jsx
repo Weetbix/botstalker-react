a320875ced2a19ca04c5b90c5a32f7e64afd2f00
John Hannagan <weetbix_@hotmail.com>
2016-07-07 00:17:59 +0200

    Extract method for cleaner codez

--- src/components/MessageList.jsx
+++ src/components/MessageList.jsx
@@ -8,26 +8,31 @@ export default class extends Component {
         usersByID : PropTypes.object
     }
 
+    renderMessages(){
+        return this.props.messages.map( msg => {
+            // Replace bot messages with a bot user
+            var user;
+            if( msg.subtype === 'bot_message' ){
+                user = {
+                    name : msg.username,
+                    profile : { image_24: 'http://emojipedia-us.s3.amazonaws.com/cache/53/df/53df16cb58da670f404ba13a6ec3a63d.png' }
+                }
+            } else {
+                user = this.props.usersByID[msg.user];
+            }
+
+            return <MessageListItem key={ msg.ts }
+                                    username={ user.name }
+                                    text={ msg.text }
+                                    avatar={ user.profile.image_24 } />
+        });
+    }
+    
     render(){
         return (
             <div className="ui segment">
                 <div className="ui list">
-                    { this.props.messages.map( msg => {
-                        // Replace bot messages with a bot user
-                        var user;
-                        if( msg.subtype === 'bot_message' ){
-                            user = {
-                                name : msg.username,
-                                profile : { image_24: 'http://emojipedia-us.s3.amazonaws.com/cache/53/df/53df16cb58da670f404ba13a6ec3a63d.png' }
-                            }
-                        } else {
-                            user = this.props.usersByID[msg.user];
-                        }
-
-                        return <MessageListItem username={ user.name }
-                                                text={ msg.text }
-                                                avatar={ user.profile.image_24 } />;
-                    })}
+                    { this.renderMessages() }
                 </div>
             </div>
         );
