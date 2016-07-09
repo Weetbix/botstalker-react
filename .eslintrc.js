eb4fa7228306b605042e138e2841cd5e06bcbbf0
John Hannagan <weetbix_@hotmail.com>
2016-07-09 14:22:13 +0200

    Update linter (use AirBnb etc)

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,34 +1,9 @@
 module.exports = {
     "parser": "babel-eslint",
+    "extends": [
+        "airbnb"
+    ],
     "rules": {
-        "strict": 0,
-        "indent": [
-            2,
-            2
-        ],
-        "quotes": [
-            2,
-            "single"
-        ],
-        "linebreak-style": [
-            2,
-            "windows"
-        ],
-        "semi": [
-            2,
-            "always"
-        ]
-    },
-    "ecmaFeatures": {
-      "modules": true,
-      "spread": true,
-      "experimentalObjectRestSpread": true,
-      "destructing": true,
-      "restParams": true
-    },
-    "env": {
-        "es6": true,
-        "browser": true
-    },
-    "extends": "eslint:recommended"
+        "indent": [2, 4]
+    }
 };
